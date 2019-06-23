087edb17f346188a30346434ed3d736f4c63efca
Moshe Praver <moshepraver@gmail.com>
2019-06-22 23:03:28 -0500

    completed remaining functions

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -13,50 +13,106 @@ function ConvertHandler() {
   let index = input.match(regex)
   if (!index) return false;
   let num = input.slice(0, index.index);
+  if (num.length === 0) return 1;
   if (isNaN(eval(num))) return false;
 
-  return num;
+  return eval(num);
   };
   
   this.getUnit = function(input) {
-    // const regex = /[a-zA-z]/
-    // const index = input.match(regex)
-    // if (!index) return false;
-    // index = index.index;
-    // if 
-    // return input.slice(0, index);
+    const regex = /[a-zA-z]/
+    let index = input.match(regex)
+    if (!index) return false;
+    let unit = input.slice(index.index);
+    if (unit !== 'gal' && unit !== 'L' && unit !== 'lbs' && unit !== 'kg' && unit !== 'mi' && unit !== 'km') return false;
+
+    return unit;
   };
   
   this.getReturnUnit = function(initUnit) {
-    const regex = /[a-zA-z]/
-    const index = input.match(regex)
-    if (!index) return false;
-  
-    index = index.index;
-    return input.slice(index);
+    switch (initUnit) {
+      case 'gal':
+        return 'L'
+      case 'L':
+        return 'gal'
+      case 'lbs':
+        return 'kg'
+      case 'kg':
+        return 'lbs'
+      case 'mi':
+        return 'km'
+      case 'km':
+        return 'mi'
+    }
   };
 
-  this.spellOutUnit = function(unit) {
-    var result;
+  // this.spellOutUnit = function(unit) {
+  //   var result;
     
-    return result;
-  };
+  //   return result;
+  // };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
     
-    return result;
-  };
-  
+    const num = eval(initNum)
+    
+    switch (initUnit) {
+      case 'gal':
+        return num * galToL
+      case 'L':
+        return num / galToL
+      case 'lbs':
+        return num * lbsToKg
+      case 'kg':
+        return  num / lbsToKg
+      case 'mi':
+        return num * miToKm
+      case 'km':
+        return num / miToKm
+    };
+  }
+
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
-  };
-  
+
+    let units = {}
+
+    switch (initUnit) {
+      case 'gal':
+        units.initial = 'gallons';
+        units.convert = 'litres'
+        break;
+      case 'L':
+        units.initial = 'litres';
+        units.convert = 'gallons'
+        break;
+      case 'lbs':
+        units.initial = 'pounds';
+        units.convert = 'kilograms'
+        break;
+      case 'kg':
+        units.initial = 'kilograms';
+        units.convert = 'pounds'
+        break;
+      case 'mi':
+        units.initial = 'miles';
+        units.convert = 'kilometers'
+        break;
+      case 'km':
+        units.initial = 'kilometers';
+        units.convert = 'miles'
+        break;
+      default: return false
+    };
+    return `${initNum} ${units.initial} converts to ${returnNum} ${units.convert}`
+  }
 }
 
 module.exports = ConvertHandler;
+
+
+
+
+
--- routes/api.js
+++ routes/api.js
@@ -24,12 +24,20 @@ module.exports = function (app) {
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
       console.log(typeof input)
-      res.json({
+      if (!initNum && !initUnit) {
+        res.send('invalid number and unit')
+      } else if (!initNum) {
+        res.send('invalid number')
+      } else if (!initUnit) {
+        res.send('invalid unit')
+      } else {
+        res.json({
         initNum,
         initUnit, 
         returnNum,
         returnUnit, 
-        string: toString});
+        string: toString
+        })
+      }      
     });
-    
 };
